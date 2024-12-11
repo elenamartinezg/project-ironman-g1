8add0694a518061228900832325ad5c71db404d6
Elena Martínez Galán <elena.martinez.galan@hotmail.com>
2024-12-11 18:10:15 +0100

    Update 11/12/2024 Notebook and Models

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pickle
 
 def create_dir(dir):
     """Create directory if it does not exists
@@ -8,3 +9,30 @@ def create_dir(dir):
     """
     if not dir.exists():
         dir.mkdir(parents=True, exist_ok=True)
+
+def load_pickle(pickle_fpath):
+    """Load Pickle File
+
+    Args:
+        pickle_fpath (Path): Input Pickle Filepath
+
+    Returns:
+        data: Loaded file
+    """
+    with open(pickle_fpath, "rb") as file:
+        data = pickle.load(file)
+    return data
+
+
+def dump_pickle(data, pickle_fpath):
+    """Dump Pickle File
+
+    Args:
+        data: Loaded file
+        pickle_fpath (Path): Output Pickle Filepath
+
+    """
+    create_dir(pickle_fpath.parent)
+    with open(pickle_fpath, "wb") as f:
+        pickle.dump(data, f)
+        
